e340c4ecdc6753204978d0099b695f15df074c68
chaddcw <chaddcw+github@gmail.com>
2014-02-10 11:35:37 -0800

    Initial Commit
    The initial commit. Creation of a driver file.

---
+++ ContactManager/ContactManager/ContactManager.cpp
@@ -0,0 +1,24 @@
+// ***************************************************************************
+// File name:	ContactManager.cpp
+// Author:		Chadd Williams
+// Date:			2/10/2014
+// Class:			CS 360
+// Assignment: Introduction to Git and Windows
+// Purpose:		To provide the driver for the contact manager
+// ***************************************************************************
+
+#include <iostream>
+
+// ***************************************************************************
+// Function:		main
+// Description: The main method for the driver. 
+//
+// Parameters: none
+//
+// Returned:		EXIT_SUCCESS on success, anything else on failure
+// ***************************************************************************
+int main()
+{
+	std::cout << "Hello World\n";
+	return EXIT_SUCCESS;
+}
